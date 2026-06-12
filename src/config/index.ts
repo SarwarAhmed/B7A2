09d13e469c9b436b4dd83b91f89b59628c17bc5c
Sarwar Ahmed <31815590+SarwarAhmed@users.noreply.github.com>
2026-06-12 14:46:51 +0600

    Database table schema

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,15 @@ import {env} from "process";
 dotenv.config({quiet: true});
 
 const config = {
+    connectionString: env.DATABASE_URL as string,
     port: env.PORT as string,
+
+    // local connection
+    user: env.DB_USER,
+    host: env.DB_HOST as string,
+    database: env.DB_NAME as string,
+    password: env.DB_PASSWORD as string,
+    db_port: env.DB_PORT,
 }
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import config from "./config";
+import {initDb} from "./db/index.db";
 const main = async () => {
+    await initDb();
     app.listen(config.port, () => console.log(`Server is running on port ${config.port}`));
 }
 
